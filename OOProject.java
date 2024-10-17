f5fe2f88985c4fb74051b37d83460b3998a72e9d
Thayu Macharia Ndegwa <135946960+MachtheG@users.noreply.github.com>
2024-10-17 09:59:54 +0300

    Create OOProject.java
    This is the main method of the program.

---
+++ OOProject.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.ooproject;
+
+/**
+ *
+ * @author machn
+ */
+public class OOProject {
+    public static void main(String[] args) {
+        // Start the Login Page for your project
+        LoginPage loginPage = new LoginPage();
+        loginPage.setVisible(true);
+    }
+}
